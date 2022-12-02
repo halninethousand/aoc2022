1ff11fd808a1d346fd92ea619b000249da161dc1
unknown <tsvetkovprelude@gmail.com>
2022-12-02 23:09:27 +0200

    day1

---
+++ src/bin/day1.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("./day1.txt");
+
+    let elves = input.split("\r\n\r\n");
+
+    let mut elf: Vec<u32> = elves
+        .map(|line| line.split("\r\n").flat_map(|num| num.parse::<u32>()).sum::<u32>())
+        .collect();
+
+    elf.sort_by(|a, b| b.cmp(a));
+
+    println!("{:?}", elf[0]);
+    println!("{:?}", elf.iter().take(3).sum::<u32>());
+}
