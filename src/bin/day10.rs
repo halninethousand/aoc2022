5b30b63a097a340682625a0d0b5de142565378e5
unknown <tsvetkovprelude@gmail.com>
2024-09-02 11:10:48 +0300

    day10 part2 draw pixel position

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -53,7 +53,74 @@ fn main() {
         }
     }
 
-    let mut machine = Machine::new(input);
+    struct Machine_part2 {
+        input: Vec<String>,
+        row_length: i32,
+        pixel_position: i32,
+        register: i32,
+        crt: String,
+    }
+
+    impl Machine_part2 {
+        fn new(input: Vec<String>) -> Self {
+            Machine_part2 {
+                input,
+                row_length: 40,
+                pixel_position: 0,
+                register: 1,
+                crt: String::from(""),
+            }
+        }
+
+        fn tick(&mut self) {
+            self.crt.push(self.get_pixel());
+            self.pixel_position += 1;
+            if self.pixel_position == self.row_length {
+                self.reset_pixel_position();
+            }
+        }
+
+        fn addx(&mut self, incr: i32) {
+            self.tick();
+            self.tick();
+            self.register += incr;
+        }
+
+        fn sprite_is_visible(&self) -> bool {
+            (self.register - 1..=self.register + 1).contains(&self.pixel_position)
+        }
+
+        fn get_pixel(&self) -> char {
+            if self.sprite_is_visible() {
+                '#'
+            } else {
+                '.'
+            }
+        }
+
+        fn reset_pixel_position(&mut self) {
+            self.crt.push('\n');
+            self.pixel_position = 0;
+        }
+
+        fn run_instructions(&mut self) {
+            for line in self.input.clone() {
+                let instruction: Vec<&str> = line.split(' ').collect();
+
+                match instruction[0] {
+                    "noop" => self.tick(),
+                    "addx" => self.addx(instruction[1].parse::<i32>().expect("Should be an int")),
+                    _ => unimplemented!(),
+                }
+            }
+        }
+    }
+
+    let mut machine = Machine::new(input.clone());
     machine.run_instructions();
     println!("signal strength sum: {:?}", machine.sum);
+
+    let mut machine_part2 = Machine_part2::new(input);
+    machine_part2.run_instructions();
+    print!("part 2 letters:\n{}", machine_part2.crt);
 }
